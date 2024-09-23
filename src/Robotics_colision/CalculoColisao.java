af4d8cf0376fc61cddc06bd0b1d38965a0ff0a8b
Pedro Bonelli <pebonellimecca@gmail.com>
2024-09-23 20:01:07 -0300

    feat: implementar função para calcular ponto de colisão

--- src/Robotics_colision/CalculoColisao.java
+++ src/Robotics_colision/CalculoColisao.java
@@ -1,9 +1,27 @@
 package Robotics_colision;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+
 public class CalculoColisao {
 
     // Função para calcular o ponto de colisão (a ser implementada aos poucos)
     public static String calcularPontoColisao(int x1, int y1, int a, int f, int d) {
-        return "";
+        // Convertendo ângulos para radianos
+        double aRad = Math.toRadians(a);  // Ângulo de orientação do robô
+        double fRad = Math.toRadians(f - 90);  // Ângulo do feixe
+
+        // Calculando o ângulo total do feixe
+        double anguloTotal = aRad + fRad;
+
+        // Calculando as coordenadas cartesianas do ponto de colisão
+        double x2 = x1 + d * cogits(anguloTotal);
+        double y2 = y1 + d * sin(anguloTotal);
+
+        // Arredondando para o inteiro mais próximo
+        int xFinal = (int) Math.round(x2);
+        int yFinal = (int) Math.round(y2);
+
+        return String.format("(%d, %d)", xFinal, yFinal);
     }
 }
